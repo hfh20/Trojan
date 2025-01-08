eaa19ca4f7f98e93b251c33c868b53ff909dbc31
hfh20 <67689643+hfh20@users.noreply.github.com>
2025-01-09 03:29:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(getenv("API_ID", "27439638"))
 API_HASH = getenv("API_HASH", "ce0b3293fb021531e92fe6341f2f1a15")
 BOT_TOKEN = getenv("BOT_TOKEN", "7433470507:AAExGKou43O1IQq0Qdk7oR3mMwL6SeCV6gE")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7899717596").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://coyegow590:gp9K5QE8uUZFITwM@cluster0.yzd39.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002287517477"))
 PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", ""))
