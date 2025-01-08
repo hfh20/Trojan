4849c5af0273c39721ec90fd5e8a5cc215a2aac5
hfh20 <67689643+hfh20@users.noreply.github.com>
2025-01-09 03:18:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "26850449"))
-API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "7091230649 6107581019").split()))
+API_ID = int(getenv("API_ID", "27439638"))
+API_HASH = getenv("API_HASH", "ce0b3293fb021531e92fe6341f2f1a15")
+BOT_TOKEN = getenv("BOT_TOKEN", "7433470507:AAExGKou43O1IQq0Qdk7oR3mMwL6SeCV6gE")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7899717596").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002287517477"))
 PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", ""))
 
 
